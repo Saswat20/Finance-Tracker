09bda2bbc4ef75364da27c4dc47bb88b63f6da3b
Saswat Kar <154300345+Saswat20@users.noreply.github.com>
2026-01-02 18:08:47 +0530

    Refactor get_transactions.php to return JSON data

--- get_transactions.php
+++ get_transactions.php
@@ -1,10 +1,20 @@
-
 <?php
-include '../config/db.php';
-$result = $conn->query("SELECT * FROM transactions");
-$data = [];
-while($row = $result->fetch_assoc()){
-    $data[] = $row;
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+include_once '../config/db.php';
+
+$sql = "SELECT * FROM transactions ORDER BY date DESC";
+$result = $conn->query($sql);
+
+$transactions = array();
+
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        array_push($transactions, $row);
+    }
 }
-echo json_encode($data);
+
+echo json_encode($transactions);
+$conn->close();
 ?>
