259ecc58391643d5b98916bf94e1f9e45796a601
Saswat Kar <154300345+Saswat20@users.noreply.github.com>
2026-01-02 18:31:09 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,4 +1,36 @@
-
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './App.css';
 import Dashboard from './components/Dashboard';
-function App(){ return <Dashboard/> }
+import TransactionForm from './components/TransactionForm';
+
+function App() {
+  const [transactions, setTransactions] = useState([]);
+
+  // Fetch data from PHP Backend
+  const fetchTransactions = () => {
+    axios.get('http://localhost/finance_tracker/backend/api/get_transactions.php')
+      .then(res => {
+        setTransactions(res.data);
+      })
+      .catch(err => console.log(err));
+  };
+
+  useEffect(() => {
+    fetchTransactions();
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="header">
+        <h1>Personal Finance Tracker</h1>
+      </header>
+      <div className="container">
+        <TransactionForm refreshData={fetchTransactions} />
+        <Dashboard transactions={transactions} />
+      </div>
+    </div>
+  );
+}
+
 export default App;
