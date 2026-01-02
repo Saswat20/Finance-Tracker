9dfc09a59a39f047d632e5bb89036eea0ab3a21f
Saswat Kar <154300345+Saswat20@users.noreply.github.com>
2026-01-02 18:08:12 +0530

    Refactor database connection initialization

--- db.php
+++ db.php
@@ -1,7 +1,12 @@
-
 <?php
-$conn = new mysqli("localhost","root","","finance_db");
-if($conn->connect_error){
-    die("DB Error");
+$host = "localhost";
+$user = "root";
+$password = ""; // Your MySQL Password
+$dbname = "finance_db";
+
+$conn = new mysqli($host, $user, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
 ?>
